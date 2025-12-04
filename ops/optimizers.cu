03667056b3f34671d3ca32b557a22eea214dbf3c
Yuval Rubin <yuvalrubinil@gmail.com>
2025-12-04 18:28:00 +0200

    Fixed RMSProp and added Adagrad

--- ops/optimizers.cu
+++ ops/optimizers.cu
@@ -33,6 +33,51 @@ void momentumCuda(Tensor& u, Tensor& v, float beta) {
     cudaDeviceSynchronize();
 }
 
+
+
+// u[i] = u[i] + (v[i] * v[i])
+__global__ void accumulatedSquers(float* u, float* v, int n) {
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    int stride = gridDim.x * blockDim.x;
+    for (; i < n; i += stride)
+        u[i] = u[i] + (v[i] * v[i]);
+}
+
+void accumulatedSquersCuda(Tensor& u, Tensor& v) {
+    int n = u.getSize();
+    int threadsPerBlock = std::min(BLOCK_MAX_SIZE, n);
+    int blocks = (n + threadsPerBlock - 1) / threadsPerBlock;
+    blocks = std::min(blocks, BLOCK_MAX_COUNT);
+    u.toDevice();
+    v.toDevice();
+    accumulatedSquers << <blocks, threadsPerBlock >> > (u.getData(), v.getData(), n);
+    cudaDeviceSynchronize();
+}
+
+//w[i] -= (learningRate / (sqrtf(v[i]) + EPSILON)) * u[i]
+__global__ void momentumSqrtNormSubtraction(float* w, float* v, float* u, int n, float learningRate) {
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    int stride = gridDim.x * blockDim.x;
+    for (; i < n; i += stride)
+        w[i] -= (learningRate / (sqrtf(v[i]) + EPSILON)) * u[i]; //w -= lr / (sqrt(vt) + eps) * (new der)
+}
+
+
+void adagradSubtractionCuda(Tensor& w, Tensor& accumulatedSquers, Tensor& newDer, float learningRate) {
+    int n = newDer.getSize();
+    int threadsPerBlock = std::min(BLOCK_MAX_SIZE, n);
+    int blocks = (n + threadsPerBlock - 1) / threadsPerBlock;
+    blocks = std::min(blocks, BLOCK_MAX_COUNT);
+    newDer.toDevice();
+    accumulatedSquers.toDevice();
+    w.toDevice();
+    momentumSqrtNormSubtraction << <blocks, threadsPerBlock >> > (w.getData(), accumulatedSquers.getData(), newDer.getData(), n, learningRate);
+    cudaDeviceSynchronize();
+}
+
+
+
+
 // u[i] = beta * u[i] + (1 - beta) * (v[i] * v[i])
 __global__ void momentumSquered(float* u, float* v, int n, float beta) {
     int i = blockIdx.x * blockDim.x + threadIdx.x;
@@ -53,21 +98,14 @@ void momentumSqueredCuda(Tensor& u, Tensor& v, float beta) {
 }
 
 
-__global__ void rmsPropSubtraction(float* w, float* v, float* u, int n, float learningRate) {
-    int i = blockIdx.x * blockDim.x + threadIdx.x;
-    int stride = gridDim.x * blockDim.x;
-    for (; i < n; i += stride)
-        w[i] -= (learningRate / (sqrtf(v[i]) + EPSILON)) * u[i]; //w -= lr / (sqrt(vt) + eps) * (new der)
-}
-
-void rmsPropSubtractionCuda(Tensor& w, Tensor& v, Tensor& u, float learningRate) {
-    int n = u.getSize();
+void rmsPropSubtractionCuda(Tensor& w, Tensor& squeredMomentum, Tensor& newDer, float learningRate) {
+    int n = newDer.getSize();
     int threadsPerBlock = std::min(BLOCK_MAX_SIZE, n);
     int blocks = (n + threadsPerBlock - 1) / threadsPerBlock;
     blocks = std::min(blocks, BLOCK_MAX_COUNT);
-    u.toDevice();
-    v.toDevice();
+    newDer.toDevice();
+    squeredMomentum.toDevice();
     w.toDevice();
-    rmsPropSubtraction << <blocks, threadsPerBlock >> > (w.getData(), v.getData(), u.getData(), n, learningRate);
+    momentumSqrtNormSubtraction << <blocks, threadsPerBlock >> > (w.getData(), squeredMomentum.getData(), newDer.getData(), n, learningRate);
     cudaDeviceSynchronize();
 }
